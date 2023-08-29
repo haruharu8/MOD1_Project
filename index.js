d191098fd17e4a8430748c771e7cecd80be5052f
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-08-29 12:21:21 -0700

    made outline

---
+++ index.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2D');
+canvas.width = 900;
+canvas.height = 600;
+
+// global variables
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+// mouse
+const mouse = {
+    x:undefined,
+    y:undefined,
+    width: 0.1,
+    height: 0.1,
+}
+
+let canvasPosition = canvas.getBoundingClientRect();
+
+// game board
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+class Cell {
+    constructor (x,y) {
+        this.x=x;
+        this.y=y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw(){
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.x,this.y,this.width,this.height);
+    }
+}
+let createGrid = () => {
+    for (let y = cellSize; y < canvas.height; y += cellSize) {
+        for (let x = cellSize; x < canvas.height; x += cellSize) {
+            gameGrid.push(new Cell(x,y));
+        }
+    }
+}
+createGrid();
+handleGameGrid = () => {
+    for (let i = 0; i < gameGrid.length; i++) {
+        gameGrid[i].draw();
+    }
+}
+
+// projectiles
+// defenders
+// enemies
+// resources 
+// utilities (animmation loop & collison detection)
+let animate = () => {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0,0,controlsBar.width, controlsBar.height);
+    requestAnimationFrame(animate);
+}
+animate();
